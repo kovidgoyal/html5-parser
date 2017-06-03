cc4c97d4ff780fbfa33aa02a75adbeb2f1076179
Kovid Goyal <kovid@kovidgoyal.net>
2017-06-03 12:54:03 +0530

    ...

--- publish.py
+++ publish.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 # vim:fileencoding=utf-8
-# License: BSD Copyright: 2017, Kovid Goyal <kovid at kovidgoyal.net>
+# License: Apache 2.0 Copyright: 2017, Kovid Goyal <kovid at kovidgoyal.net>
 
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
