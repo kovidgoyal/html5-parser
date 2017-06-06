3c9e757bb56f422f6e442ad519bb040c389f28e1
Kovid Goyal <kovid@kovidgoyal.net>
2017-06-06 11:24:27 +0530

    Make try more convenient by pre-importing

--- build.py
+++ build.py
@@ -2,8 +2,7 @@
 # vim:fileencoding=utf-8
 # License: Apache 2.0 Copyright: 2017, Kovid Goyal <kovid at kovidgoyal.net>
 
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+from __future__ import (absolute_import, division, print_function, unicode_literals)
 
 import argparse
 import errno
@@ -32,11 +31,11 @@ PKGCONFIG = os.environ.get('PKGCONFIG_EXE', 'pkg-config')
 with open(os.path.join(base, 'src/gumbo-libxml.c'), 'rb') as f:
     raw = f.read().decode('utf-8')
 version = tuple(
-    map(int, (re.search(
-        r'^#define MAJOR (\d+)', raw, flags=re.MULTILINE).group(1), re.search(
-            r'^#define MINOR (\d+)', raw,
-            flags=re.MULTILINE).group(1), re.search(
-                r'^#define PATCH (\d+)', raw, flags=re.MULTILINE).group(1), )))
+    map(
+        int, (
+            re.search(r'^#define MAJOR (\d+)', raw, flags=re.MULTILINE).group(1), re.search(
+                r'^#define MINOR (\d+)', raw, flags=re.MULTILINE).group(1), re.search(
+                    r'^#define PATCH (\d+)', raw, flags=re.MULTILINE).group(1), )))
 
 
 def safe_makedirs(path):
@@ -48,8 +47,7 @@ def safe_makedirs(path):
 
 
 def add_python_flags(env, return_libs=False):
-    env.cflags.extend('-I' + sysconfig.get_path(x)
-                      for x in 'include platinclude'.split())
+    env.cflags.extend('-I' + sysconfig.get_path(x) for x in 'include platinclude'.split())
     libs = []
     libs += sysconfig.get_config_var('LIBS').split()
     libs += sysconfig.get_config_var('SYSLIBS').split()
@@ -59,20 +57,15 @@ def add_python_flags(env, return_libs=False):
             val = sysconfig.get_path(var)
             if val and '/{}.framework'.format(fw) in val:
                 fdir = val[:val.index('/{}.framework'.format(fw))]
-                if os.path.isdir(
-                        os.path.join(fdir, '{}.framework'.format(fw))):
+                if os.path.isdir(os.path.join(fdir, '{}.framework'.format(fw))):
                     framework_dir = fdir
                     break
         else:
             raise SystemExit('Failed to find Python framework')
-        libs.append(
-            os.path.join(framework_dir, sysconfig.get_config_var('LDLIBRARY')))
+        libs.append(os.path.join(framework_dir, sysconfig.get_config_var('LDLIBRARY')))
     else:
         libs += ['-L' + sysconfig.get_config_var('LIBDIR')]
-        libs += [
-            '-lpython' + sysconfig.get_config_var('VERSION') + getattr(
-                sys, 'abiflags', '')
-        ]
+        libs += ['-lpython' + sysconfig.get_config_var('VERSION') + getattr(sys, 'abiflags', '')]
         libs += sysconfig.get_config_var('LINKFORSHARED').split()
     env.ldflags.extend(libs)
     return libs if return_libs else env
@@ -110,14 +103,11 @@ def library_dirs():
 
 def cc_version():
     cc = os.environ.get('CC', 'gcc')
-    raw = subprocess.check_output(
-        [cc, '-dM', '-E', '-'], stdin=open(os.devnull, 'rb'))
+    raw = subprocess.check_output([cc, '-dM', '-E', '-'], stdin=open(os.devnull, 'rb'))
     m = re.search(br'^#define __clang__ 1', raw, flags=re.M)
     cc_name = 'gcc' if m is None else 'clang'
-    ver = int(re.search(br'#define __GNUC__ (\d+)', raw, flags=re.M)
-              .group(1)), int(
-                  re.search(br'#define __GNUC_MINOR__ (\d+)', raw, flags=re.M)
-                  .group(1))
+    ver = int(re.search(br'#define __GNUC__ (\d+)', raw, flags=re.M).group(1)), int(
+        re.search(br'#define __GNUC_MINOR__ (\d+)', raw, flags=re.M).group(1))
     return cc, ver, cc_name
 
 
@@ -135,12 +125,8 @@ def get_sanitize_args(cc, ccver):
     return sanitize_args
 
 
-def init_env(debug=False,
-             sanitize=False,
-             native_optimizations=False,
-             add_python=True):
-    native_optimizations = (native_optimizations and not sanitize and
-                            not debug)
+def init_env(debug=False, sanitize=False, native_optimizations=False, add_python=True):
+    native_optimizations = (native_optimizations and not sanitize and not debug)
     cc, ccver, cc_name = cc_version()
     stack_protector = '-fstack-protector'
     if ccver >= (4, 9) and cc_name == 'gcc':
@@ -151,18 +137,15 @@ def init_env(debug=False,
     optimize = '-ggdb' if debug or sanitize else '-O3'
     sanitize_args = get_sanitize_args(cc_name, ccver) if sanitize else set()
     cflags = os.environ.get(
-        'OVERRIDE_CFLAGS',
-        ('-Wextra -Wno-missing-field-initializers -Wall -std=c99'
-         ' -pedantic-errors -Werror {} {} -D{}DEBUG -fwrapv {} {} -pipe {}'
-         ).format(optimize, ' '.join(sanitize_args), ('' if debug else 'N'),
-                  stack_protector, missing_braces, '-march=native'
-                  if native_optimizations else ''))
+        'OVERRIDE_CFLAGS', (
+            '-Wextra -Wno-missing-field-initializers -Wall -std=c99'
+            ' -pedantic-errors -Werror {} {} -D{}DEBUG -fwrapv {} {} -pipe {}').format(
+                optimize, ' '.join(sanitize_args), ('' if debug else 'N'), stack_protector,
+                missing_braces, '-march=native' if native_optimizations else ''))
     libxml_cflags = pkg_config('libxml-2.0', '--cflags')
-    cflags = shlex.split(cflags) + libxml_cflags + shlex.split(
-        sysconfig.get_config_var('CCSHARED'))
-    ldflags = os.environ.get('OVERRIDE_LDFLAGS',
-                             '-Wall -shared ' + ' '.join(sanitize_args) +
-                             ('' if debug else ' -O3'))
+    cflags = shlex.split(cflags) + libxml_cflags + shlex.split(sysconfig.get_config_var('CCSHARED'))
+    ldflags = os.environ.get(
+        'OVERRIDE_LDFLAGS', '-Wall -shared ' + ' '.join(sanitize_args) + ('' if debug else ' -O3'))
     libxml_ldflags = pkg_config('libxml-2.0', '--libs')
     ldflags = shlex.split(ldflags) + libxml_ldflags
     cflags += shlex.split(os.environ.get('CFLAGS', ''))
@@ -207,8 +190,7 @@ def find_c_files(src_dir):
 
 def build_obj(src, env, headers):
     suffix = '-debug' if env.debug else ''
-    obj = os.path.join(
-        build_dir, os.path.basename(src).rpartition('.')[0] + suffix + '.o')
+    obj = os.path.join(build_dir, os.path.basename(src).rpartition('.')[0] + suffix + '.o')
     if newer(obj, src, *headers):
         cflags = list(env.cflags)
         if src.endswith('char_ref.c'):
@@ -220,8 +202,7 @@ def build_obj(src, env, headers):
 
 TEST_EXE = os.path.join(build_dir, 'test')
 if is_travis:
-    TEST_EXE = os.path.join(
-        os.path.dirname(os.path.abspath(sys.executable)), 'test-html5-parser')
+    TEST_EXE = os.path.join(os.path.dirname(os.path.abspath(sys.executable)), 'test-html5-parser')
 SRC_DIRS = 'src gumbo'.split()
 MOD_EXT = '.so'
 
@@ -245,8 +226,7 @@ def build(args):
         debug_objects.extend(build_obj(c, debug_env, headers) for c in sources)
     link(debug_objects, debug_env)
     ldflags = add_python_flags(deepcopy(debug_env), return_libs=True)
-    cmd = ([debug_env.cc] + debug_env.cflags + ['test.c'] + ['-o', TEST_EXE] +
-           ldflags)
+    cmd = ([debug_env.cc] + debug_env.cflags + ['test.c'] + ['-o', TEST_EXE] + ldflags)
     if newer(TEST_EXE, *debug_objects):
         run_tool(cmd)
     for mod in glob.glob(os.path.join(build_dir, '*' + MOD_EXT)):
@@ -293,7 +273,8 @@ def main():
         build(args)
         os.environ['ASAN_OPTIONS'] = 'leak_check_at_exit=0'
         add_python_path(os.environ, os.path.dirname(freeze_dir))
-        os.execlp(TEST_EXE, TEST_EXE, '-c', args.rest[0], *args.rest[1:])
+        os.execlp(
+            TEST_EXE, TEST_EXE, '-c', 'from html5_parser import *; ' + args.rest[0], *args.rest[1:])
 
 
 if __name__ == '__main__':
