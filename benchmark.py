d42564e4af0544a8d9cd18f4761c77b8b83bc008
Kovid Goyal <kovid@kovidgoyal.net>
2017-06-20 13:10:17 +0530

    Add some more comparisons to the benchmark
    Fixes #1

--- benchmark.py
+++ benchmark.py
@@ -8,6 +8,7 @@ import argparse
 from functools import partial
 
 import html5lib
+from bs4 import BeautifulSoup
 
 import html5_parser
 
@@ -51,11 +52,15 @@ def doit(name, func, num=20):
 
 p = argparse.ArgumentParser(description='Benchmark html5-parser')
 p.add_argument('treebuilder', nargs='?', default='lxml', choices='lxml soup dom etree'.split())
-p.add_argument('--num', '-n', default=20, type=int,
-               help='Number of repetitions for html5lib (html5-parser will use 5x as many reps)')
+p.add_argument(
+    '--num',
+    '-n',
+    default=10,
+    type=int,
+    help='Number of repetitions for html5lib (html5-parser will use 10x as many reps)')
 args = p.parse_args()
 
-t1 = doit(
+base_time = doit(
     'html5-parser',
     partial(
         html5_parser.parse,
@@ -63,9 +68,41 @@ t1 = doit(
         transport_encoding="utf-8",
         namespace_elements=True,
         treebuilder=args.treebuilder),
-    num=args.num * 5)
-t2 = doit(
+    num=args.num * 10)
+soup_time = doit(
+    'html5-parser-to-soup',
+    partial(html5_parser.parse, raw, transport_encoding="utf-8", treebuilder='soup'),
+    num=args.num)
+
+h5time = doit(
     'html5lib',
     partial(html5lib.parse, raw, transport_encoding="utf-8", treebuilder=args.treebuilder),
     num=args.num)
-print('Speedup: {}x'.format(round(t2 / t1)))
+soup5_time = doit(
+    'BeautifulSoup-with-html5lib', partial(BeautifulSoup, raw, 'html5lib'), num=args.num)
+soup4_time = doit('BeautifulSoup-with-lxml', partial(BeautifulSoup, raw, 'lxml'), num=args.num)
+
+
+def row(*args):
+    for a in args:
+        print('{:18s}'.format(str(a)), end='|')
+    print()
+
+
+print()
+print(
+    'Results are below. They show how much faster html5-parser is than'
+    ' each specified parser. Note that there are two additional considerations:'
+    ' what the final tree is and whether the parsing supports the HTML 5'
+    ' parsing algorithm. The most apples-to-apples comparison is when the'
+    ' final tree is lxml and HTML 5 parsing is supported by the parser being compared to.'
+    ' Note that in this case, we have the largest speedup. In all other cases,'
+    ' speedup is less because of the overhead of building the final tree'
+    ' in python instead of C or because the compared parser does not use'
+    ' the HTML 5 parsing algorithm or both.')
+print()
+row('Parser', 'Tree', 'Supports HTML 5', 'Speedup (factor)')
+print('=' * 79)
+row('html5lib', 'lxml', 'yes', round(h5time / base_time))
+row('soup+html5lib', 'BeautifulSoup', 'yes', round(soup5_time / soup_time))
+row('soup+lxml.html', 'BeautifulSoup', 'no', round(soup4_time / soup_time))
